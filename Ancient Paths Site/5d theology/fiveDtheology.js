bbe1f1968d4665057431928968b88a3601e47120
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-11-29 01:51:05 +0300

    Adding 5d theollogy sub site

---
+++ Ancient Paths Site/5d theology/fiveDtheology.js
@@ -0,0 +1,95 @@
+////////////////////////////////SANCTUARY ITEMS/////////////////////////////////////////
+let sacrificeAltar = {
+  Literal_Isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+
+let lavaBasin = {
+  Literal_Isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+
+let incenseAltar = {
+  Literal_Isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+
+let menorah = {
+  Literal_Isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+let shewBread = {
+  Literal_Isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+let arkConvenant = {
+  Literal_Isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+
+let templeAsResidence = {
+  Literal_Isreal: "Temple of God and Shekinah  among 12 tribes",
+  Christ: "Christ is the  Temple among 12 disciples",
+  Christians: "We are Temple Holy Spirit Resides in Us",
+  Church: "Entire Church is Temple in which God Dwells",
+  Heaven: "There is a heavenly Temple",
+};
+let temple = {
+  Literal_Isreal: "Literal Temple",
+  Christ: "Christ is the  Temple among 12 disciples",
+  Christians: "Holy SpiritResides in Us",
+  Church: "Temple of God",
+  Heaven: "Temple of God",
+};
+
+let templeLatterRain = {
+  Literal_Isreal: "Holy Spirt entered Temple when it was complete",
+  Christ:
+    "Christ came at eh fullfiled time in 70 weeks prophecy to dwell among his people",
+  Christians: "Holy Spirit filled Church at PenteCost",
+  Church: "Holy Spirit will fill Whole Church at Latter Rain",
+  Heaven: "Temple of God",
+};
+let lamb = {
+  Literal_Isreal: "Bring Lmab for Sacrifice",
+  Christ: "Lamb of God",
+  Christians: "Believe on lamb for Salvation",
+  Church: "Saved by Lamb",
+  Heaven: "Saved by Lamb",
+};
+
+////////////////////////////////PATHS TO SALVATON AND SALVATION ITEMS/////////////////////////////////////////
+let = {
+  isreal: "Sons of God",
+  Christ: "The Sone of God",
+  Christians: "Children of God",
+  Church: "altar of shew Bread",
+  Heaven: "Lava Basin",
+};
+
+////////////////////////////////RECREATION AND PROMISED LAND/////////////////////////////////////////
+let promisedLand = {
+  isreal: "Jerusalem",
+  Christ: "New Jerusalem.New Earth and New Heaven",
+  Christians: "New Jerusalem/New Earth and New Heaven",
+  Church: "New Hevaen and New Earth",
+  Heaven: "New Hevaen and New Earth",
+};
